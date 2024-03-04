80f0bbb7a0c6926c33acf2db16d7f5f0bb01cac9
Samar <iamalsksamar@gmail.com>
2024-03-04 22:57:55 +0000

    working

--- app/src/main/java/com/example/scannertest/MainActivity.kt
+++ app/src/main/java/com/example/scannertest/MainActivity.kt
@@ -16,8 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.scannertest.ScannerUtility.registerForScanData
-import com.example.scannertest.ScannerUtility.unregisterForScanData
 import com.example.scannertest.ui.theme.ScannerTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +23,6 @@ class MainActivity : ComponentActivity() {
     var barcodeData by mutableStateOf("No Data")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        registerForScanData()
         setContent {
             ScannerTestTheme {
                 // A surface container using the 'background' color from the theme
@@ -51,9 +48,7 @@ class MainActivity : ComponentActivity() {
             )
             Button(
                 onClick = {
-
-
-                    //startActivity( Intent(this@MainActivity,ScannerActivity::class.java))
+                    startActivity( Intent(this@MainActivity,ScannerActivity::class.java))
                 }
             ) {
                 Text(text = "Scan")
@@ -74,11 +69,6 @@ class MainActivity : ComponentActivity() {
         intent?.let {
             barcodeData = it.getStringExtra("barcodeData") ?: "No Data"
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterForScanData()
     }
 
 }
